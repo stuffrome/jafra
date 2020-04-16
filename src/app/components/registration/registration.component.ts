514b1f1fb4921f846512e10d30b04a5d856d44b5
stuffrome <gabeperez018@gmail.com>
2020-04-16 15:19:36 -0400

    Registration fixed

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {UserService} from '../../services/user.service';
 import {AuthenticationService} from '../../services/auth/authentication.service';
 import {Router} from '@angular/router';
+import {delay} from 'rxjs/operators';
 
 @Component({
   selector: 'app-registration',
@@ -10,10 +11,20 @@ import {Router} from '@angular/router';
 })
 export class RegistrationComponent implements OnInit {
 
+  DEFAULT_SIGNUP_TEXT = 'SIGN UP';
+
+  signUpBtnText: string;
+
+  failedRegistration: boolean;
+  errorMessage: string;
+
   constructor(
     private userService: UserService,
     private authenticationService: AuthenticationService,
-    private router: Router) { }
+    private router: Router) {
+    this.failedRegistration = false;
+    this.signUpBtnText = this.DEFAULT_SIGNUP_TEXT;
+  }
 
   ngOnInit() {
     this.loggedInCheck();
@@ -26,9 +37,16 @@ export class RegistrationComponent implements OnInit {
   }
 
   register(username: string, email: string, password: string, passwordConfirm: string) {
-    // Validation
     if (password === passwordConfirm) {
-      this.userService.createUser(username, email, password);
+      this.signUpBtnText = 'SIGNING UP...'
+      this.userService.createUser(username, email, password).subscribe(res => {
+        // Successful registration
+        this.router.navigateByUrl('login');
+      }, err => {
+        this.errorMessage = err.error.message;
+        this.failedRegistration = true;
+        this.signUpBtnText = this.DEFAULT_SIGNUP_TEXT;
+      });
     }
   }
 
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {ConfigurationService} from './config/configuration.service';
+import {Observable} from 'rxjs';
 
 class UserAccountRequest {
   username: string;
@@ -27,9 +28,9 @@ export class UserService {
     this.userUrl = config.backendUrl() + '/users';
   }
 
-  public createUser(username: string, email: string, password: string) {
+  public createUser(username: string, email: string, password: string): Observable<any> {
     const newUserAccount = new UserAccountRequest(username, email, password);
 
-    this.http.post(this.userUrl + '/create', newUserAccount).subscribe();
+    return this.http.post(this.userUrl + '/create', newUserAccount);
   }
 }
